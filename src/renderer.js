53845bc41f46f391c0524817fd9d217f960208e4
hitgavin <zhangshuohit@163.com>
2023-12-07 16:35:50 +0800

    tmp

--- src/renderer.js
+++ src/renderer.js
@@ -1,14 +1,12 @@
-
 const { configureStore } = require("@reduxjs/toolkit");
 const React = require("react");
 const ReactDOM = require("react-dom");
 const { Provider } = require("react-redux");
 const NotebookApp = require("@nteract/notebook-app-component");
-const { applyMiddleware, createStore } = require("redux");
-const { createContentRef } = require("@nteract/core");
-const { reducer, initialState, contentRef } = require("./json-reducer");
 const { createEpicMiddleware } = require("redux-observable");
 const { contents } = require("rx-jupyter");
+
+const { reducer, initialState, contentRef } = require("./json-reducer");
 
 const epicMiddleware = createEpicMiddleware({ dependencies: contents.JupyterContentProvider });
 
@@ -18,11 +16,11 @@ const store = configureStore({
   middleware: [epicMiddleware],
 });
 
-const App = () => (
-  <Provider store={store}>
-    <NotebookApp contentRef={contentRef} />
-  </Provider>
+const App = () => React.createElement(
+  Provider,
+  { store: store },
+  React.createElement(NotebookApp, { contentRef: contentRef })
 );
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(React.createElement(App), rootElement);
