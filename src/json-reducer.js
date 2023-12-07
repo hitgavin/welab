cc687f9d55f3f5b9634989e9de2834b5d6f50d8f
hitgavin <zhangshuohit@163.com>
2023-12-07 16:07:01 +0800

    tmp

---
+++ src/json-reducer.js
@@ -0,0 +1,66 @@
+const Immutable = require("immutable");
+const { makeEntitiesRecord, makeContentsRecord } = require("@nteract/core");
+const { createContentRef, createKernelspecsRef, makeNotebookContentRecord } = require("@nteract/core");
+
+const contentRef = createContentRef();
+const kernelspecsRef = createKernelspecsRef();
+
+const initialRefs = Immutable.Map().set(
+  contentRef,
+  makeNotebookContentRecord()
+);
+
+const initialState = {
+  core: makeStateRecord({
+    currentKernelspecsRef: kernelspecsRef,
+    entities: makeEntitiesRecord({
+      contents: makeContentsRecord({
+        byRef: initialRefs
+      })
+    })
+  }),
+  config: Immutable.Map({
+    theme: "light"
+  })
+};
+
+const reducer = (state, action) => {
+  const newState = { ...state };
+  switch (action.type) {
+    case "CREATE": {
+      // we create a new object
+      newState[action.payload.id] = action.payload;
+      return newState;
+    }
+    // case "RETRIEVE": let selection = state[id];
+    case "UPDATE": {
+      // we do nothing if the object does not exist
+      if (!Object.keys(state).includes(action.payload.id)) {
+        return state;
+      }
+      let newObject = Object.assign(action.payload.id);
+      if (action.payload.event) {
+        // we update a property
+        let { name, value } = action.payload.event.target;
+        newObject[name] = value;
+      } else {
+        // we replace the object
+        newObject = action.payload;
+      }
+      newState[action.payload.id] = newObject;
+      return newState;
+    }
+    case "DELETE": {
+      // we delete the objext
+      if (!action.payload.id) {
+        delete newState[action.payload];
+      } else delete newState[action.payload.id];
+      return newState;
+    }
+    default: {
+      console.warn("Unhandled Action of type ", action.type);
+      console.log(action.payload);
+      return newState;
+    }
+  }
+};
--- src/renderer.js
+++ src/renderer.js
@@ -1,55 +1,27 @@
-/**
- * @file This file contains the code for rendering the React application and initializing the Jupyter kernel connection.
- */
+import { configureStore } from "@reduxjs/toolkit";
+const React = require("react");
+const ReactDOM = require("react-dom");
+const { Provider } = require("react-redux");
+const NotebookApp = require("@nteract/notebook-app-component");
+const { applyMiddleware, createStore } = require("redux");
+const { createContentRef } = require("@nteract/core");
+const { reducer, initialState, contentRef } = require("./json-reducer");
+const { createEpicMiddleware } = require("redux-observable");
+const { contents } = require("rx-jupyter");
 
-const React = require('react');
-const ReactDOM = require('react-dom');
-const { Provider } = require('react-redux');
-const { combineReducers } = require('redux');
-const { NotebookApp } = require('@nteract/notebook-app-component');
-const { kernels, notebooks, transforms } = require('@nteract/core');
-const { configureStore } = require('@reduxjs/toolkit');
-
-/**
- * Combines the reducers for kernels, notebooks, and transforms into a single root reducer.
- * @type {Function}
- */
-const rootReducer = combineReducers({
-  kernels,
-  notebooks,
-  transforms
-});
-
-/**
- * The initial state for the core reducer.
- * @type {Object}
- */
-const initialCoreState = {
-  kernels: kernels.makeStateRecord(),
-  notebooks: notebooks.makeStateRecord()
-};
-
-/**
- * Creates the Redux store with the root reducer and initial state.
- * @type {Object}
- */
+const epicMiddleware = createEpicMiddleware({ dependencies: contents.JupyterContentProvider });
 
 const store = configureStore({
-  reducer: rootReducer,
-  preloadedState: { core: initialCoreState }
+  reducer,
+  preloadedState: initialState,
+  middleware: [epicMiddleware],
 });
 
-// initialize the Jupyter kernel connection
-store.dispatch(kernels.launchKernelByName({ kernelSpecName: 'python3', cwd: '.' }));
-
-/**
- * The main React component that wraps the application with the Redux Provider.
- * @returns {JSX.Element} The rendered React component.
- */
 const App = () => (
-  React.createElement(Provider, { store },
-    React.createElement(NotebookApp)
-  )
+  <Provider store={store}>
+    <NotebookApp contentRef={contentRef} />
+  </Provider>
 );
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
