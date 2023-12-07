bcacbfc39594e18c3c9eec9df69ca2361c769427
hitgavin <zhangshuohit@163.com>
2023-12-07 18:44:11 +0800

    tmp

--- src/renderer.js
+++ src/renderer.js
@@ -3,24 +3,31 @@ const React = require("react");
 const ReactDOM = require("react-dom");
 const { Provider } = require("react-redux");
 const NotebookApp = require("@nteract/notebook-app-component");
-const { reducers, createContentRef } = require("@nteract/core");
+const { makeEntitiesRecord, makeContentsRecord, makeStateRecord } = require("@nteract/core");
+const { reducers, createContentRef, createKernelspecsRef, makeNotebookContentRecord } = require("@nteract/core");
 const { createEpicMiddleware } = require("redux-observable");
 const { contents } = require("rx-jupyter");
 
 const epicMiddleware = createEpicMiddleware({ dependencies: contents.JupyterContentProvider });
 
+const contentRef = createContentRef();
+const kernelspecsRef = createKernelspecsRef();
+
+const initialRefs = Immutable.Map().set(
+  contentRef,
+  makeNotebookContentRecord()
+);
+
 const initialState = {
-  core: {
-    entities: {
-      contents: {},
-      kernels: {},
-      kernelspecs: {}
-    }
-  },
-  config: {
-    // 这里可以放置一些全局配置选项
-  },
-};
+  core: makeStateRecord({
+    currentKernelspecsRef: kernelspecsRef,
+    entities: makeEntitiesRecord({
+      contents: makeContentsRecord({
+        byRef: initialRefs
+      })
+    })
+  })
+}
 
 const store = configureStore({
   reducer: reducers,
@@ -28,8 +35,6 @@ const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }).concat(epicMiddleware)
 });
-
-const contentRef = createContentRef();
 
 const App = () => React.createElement(
   Provider,
